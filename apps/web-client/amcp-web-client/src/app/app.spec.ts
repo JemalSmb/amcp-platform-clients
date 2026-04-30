33792d146d2343a603e64ce6c4d94fbb9e4cdc47
Jemal Samba <jamalsamba@gmail.com>
2026-04-30 14:46:10 +0100

    web: update drawer nav and config

--- apps/web-client/amcp-web-client/src/app/app.spec.ts
+++ apps/web-client/amcp-web-client/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, amcp-web-client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('User Drawer');
   });
 });
